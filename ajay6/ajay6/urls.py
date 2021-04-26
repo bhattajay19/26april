6131aa4c45f282ed6314919f2dfebb030a9b54f0
Ajay Bhatt <bhattajay88@gmail.com>
2021-04-26 17:22:28 +0530

    Initial Commit

---
+++ ajay6/ajay6/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import  obtain_auth_token
+# from api.auth import CustomAuthToken
+
+
+#creating router objects
+router = DefaultRouter()
+
+
+#resgister StudentViewSet with Router
+
+router.register('studentapi',views.StudentModelViewSet,basename='student')
+
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('',include(router.urls)),
+    path('auth/',include('rest_framework.urls',namespace='rest_framework')),
+    # path('gettoken/',CustomAuthToken.as_view())
+
+]
